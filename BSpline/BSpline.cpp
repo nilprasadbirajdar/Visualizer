774411c2fd6726531d1b576b85218c10cbc9ebae
nilprasadbirajdar <149322004+nilprasadbirajdar@users.noreply.github.com>
2023-12-20 00:22:44 +0530

    Add files via upload

---
+++ BSpline/BSpline.cpp
@@ -0,0 +1,22 @@
+// BSpline.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "BSpline.h"
+
+
+// This is an example of an exported variable
+BSPLINE_API int nBSpline=0;
+
+// This is an example of an exported function.
+BSPLINE_API int fnBSpline(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+BSpline::BSpline()
+{
+    return;
+}
---
+++ BSpline/BSpline.h
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+// This class is exported from the dll
+class BSPLINE_API BSpline {
+public:
+	BSpline(void);
+	// TODO: add your methods here.
+};
+
+extern BSPLINE_API int nBSpline;
+
+BSPLINE_API int fnBSpline(void);
